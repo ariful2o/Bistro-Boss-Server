5544d4d5b3b7be7aa3baea28f9246340301d392c
mohammadariful20 <arifulz848@gmail.com>
2024-06-19 19:50:11 +0600

    check admin and verify token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,6 +21,21 @@ const client = new MongoClient(uri, {
   },
 });
 
+//custom middleware
+const verify = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "forbidden" });
+  }
+  // verify a token symmetric
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +47,21 @@ async function run() {
     const reviewsCollection = database.collection("reviews");
     const addtoCartCollection = database.collection("addtoCart");
     const usersCollection = database.collection("users");
+
+    //genate a secret key require('crypto').randomBytes(64).toString('hex')
+
+    //jwt related api methods
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(
+        {
+          user,
+        },
+        process.env.ACESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ token });
+    });
 
     //user related api methods
     app.post("/users", async (req, res) => {
@@ -44,22 +75,30 @@ async function run() {
         res.send(result);
       }
     });
-    app.get("/users", async (req, res) => {
+
+    app.get("/users/admim/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const result = await usersCollection.find(query).toArray();
+      const isAdmin = result.find((user) => user.role === "admin");
+      isAdmin ? res.send(true) : res.send(false);
+    });
+    
+    app.get("/users", verify, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
-    app.patch("/users/:id",async(req,res)=>{
+    app.patch("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await usersCollection.updateOne(query,{$set:req.body});
+      const result = await usersCollection.updateOne(query, { $set: req.body });
       res.send(result);
-    })
-    app.delete('/users/:id',async(req,res)=>{
+    });
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await usersCollection.deleteOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Get the database
     app.get("/menu", async (req, res) => {
